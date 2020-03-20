c524f784d490cf06e3772adca965207033640bfa
leicareddot <leicareddot@gmail.com>
2020-03-21 04:51:30 +0900

    Spring Boot - HttpMessageConverter

---
+++ spring-boot-and-spring-web-mvc/src/test/java/com/atoz_develop/springbootandspringwebmvc/SpringBootAndSpringWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atoz_develop.springbootandspringwebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAndSpringWebMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ spring-boot-and-spring-web-mvc/src/test/java/com/atoz_develop/springbootandspringwebmvc/user/UserControllerTest.java
@@ -0,0 +1,45 @@
+package com.atoz_develop.springbootandspringwebmvc.user;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"));
+    }
+
+    @Test
+    public void createUser_JSON() throws Exception {
+        String userJson = "{\"username\":\"leica\", \"password\":\"1234\"}";
+
+        mockMvc.perform(post("/users/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(userJson))
+
+                // 응답 확인
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username", is(equalTo("leica"))))
+                .andExpect(jsonPath("$.password", is(equalTo("1234"))));
+    }
+}
