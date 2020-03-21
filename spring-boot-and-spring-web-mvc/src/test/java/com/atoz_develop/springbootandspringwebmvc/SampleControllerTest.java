6753d1311c0543f7476ba9ef91de3a4125db3704
leicareddot <leicareddot@gmail.com>
2020-03-22 08:53:36 +0900

    Spring Boot - HtmlUnit 예제

--- spring-boot-and-spring-web-mvc/src/test/java/com/atoz_develop/springbootandspringwebmvc/SampleControllerTest.java
+++ spring-boot-and-spring-web-mvc/src/test/java/com/atoz_develop/springbootandspringwebmvc/SampleControllerTest.java
@@ -1,5 +1,8 @@
 package com.atoz_develop.springbootandspringwebmvc;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -28,5 +32,16 @@ public class SampleControllerTest {
                 .andExpect(view().name("hello"))    // view 이름 확인
                 .andExpect(model().attribute("name", is("leica"))) // model - name=leica
                 .andExpect(content().string(containsString("leica")));
+    }
+
+    // HtmlUnit을 사용한 테스트
+    @Autowired
+    WebClient webClient;
+
+    @Test
+    public void helloWithHtmlUnit() throws Exception {
+        HtmlPage page = webClient.getPage("/hello/thymeleaf");
+        HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+        assertThat(h1.getTextContent()).isEqualToIgnoringCase("leica");
     }
 }
