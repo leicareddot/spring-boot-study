fb6930358a2f57095365da53bc19f9fe290d65a9
leicareddot <leicareddot@gmail.com>
2020-03-22 08:58:06 +0900

    Spring Boot - spring boot dev tools

---
+++ spring-boot-dev-tools/src/test/java/com/atoz_develop/springbootdevtools/SpringBootDevToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atoz_develop.springbootdevtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDevToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
